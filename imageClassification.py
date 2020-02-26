9caba17a57b0c063fbe134ba22f821552d48409f
Mavunga Prosper Julius <prmavhunga@gmail.com>
2020-02-26 10:05:12 +0100

    Update imageClassification.py

--- imageClassification.py
+++ imageClassification.py
@@ -1,4 +1,36 @@
-import tensorflow as tf
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10)
+])import tensorflow as tf
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+fashion_mnist = keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+#model defination and layer definition
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28, 28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10)
+])
+#Optimizer definiation and matics definition
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+#code for training the model
+model.fit(train_images, train_labels, epochs=10)
+
+
+# the below lines of code are used for image classification.
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+
+
+
+predictions = probability_model.predict(test_images)
