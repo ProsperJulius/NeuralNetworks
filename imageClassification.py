225fa87fded9e0036651f266e63cd32041b5b464
Mavunga Prosper Julius <prmavhunga@gmail.com>
2020-02-26 15:51:26 +0100

    Update imageClassification.py

--- imageClassification.py
+++ imageClassification.py
@@ -25,6 +25,29 @@ model.compile(optimizer='adam',
 
 #code for training the model
 model.fit(train_images, train_labels, epochs=10)
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from tensorflow import keras
+
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+train_images = train_images/255.0
+test_images = test_images/255.0
+model = keras.Sequential([
+                       keras.layers.Flatten(input_shape=(28,28)),
+                       keras.layers.Dense(units=200,activation='relu'),
+                       keras.layers.Dense(units=10,activation='softmax')  
+])
+
+model.compile(optimizer="adam",loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(train_images, train_labels, epochs=10)
+test_l, test_ac = model.evaluate(test_images,test_labels, verbose=2)
+model.summary()
 
 
 # the below lines of code are used for image classification.
